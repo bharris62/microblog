313832666101b5ed886c12ddcd8efcce0905e934
Blake Harris <blakebharris@gmail.com>
2016-09-27 19:47:33 -0700

    added some fake posts and logic in templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'nickname': 'bharris62'}  # fake user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Sun Drop is the best drink ever!'
+        },
+        {
+            'author': {'nickname': 'Jane'},
+            'body': 'Worst. Movie. Ever!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
